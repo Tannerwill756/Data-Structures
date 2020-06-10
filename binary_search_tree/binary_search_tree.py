8fdc561de495c256a06dd9ed117631d314a86e50
Tanner Williams <tannerwill756@gmail.com>
2020-06-10 17:54:25 -0400

    contains working

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,6 +9,8 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+
+
 class BSTNode:
     def __init__(self, value):
         self.value = value
@@ -17,14 +19,46 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # 1. check if there is no root
+        if self is None:
+            # if there isn't, create the node and park it there
+            self = BSTNode(value)
+        # 2. otherwise, there is a root
+        else:
+            # compare teh value to the root's value to determine which direction we go in
+            # if the value < roots value
+            if value < self.value:
+                # go left
+                if self.left:
+                    self.left.insert(value)
+                else:
+                    self.left = BSTNode(value)
+            # if the value > roots value
+            else:
+                # go right
+                if self.right:
+                    self.right.insert(value)
+                else:
+                    self.right = BSTNode(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if self.value == target:
+            return True
+        if target > self.value:
+            if self.right:
+                self.right.contains(target)
+            else:
+                return False
+        if target < self.value:
+            if self.left:
+                self.left.contains(target)
+            else:
+                return False
 
     # Return the maximum value found in the tree
+
     def get_max(self):
         pass
 
@@ -59,3 +93,11 @@ class BSTNode:
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
         pass
+
+
+bst = BSTNode(5)
+bst.insert(2)
+bst.insert(3)
+bst.insert(7)
+bst.insert(6)
+bst.contains(6)
