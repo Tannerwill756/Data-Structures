7308363ef3297cf32fe758a8d7a1e77369bee5ac
Tanner Williams <tannerwill756@gmail.com>
2020-06-10 18:47:40 -0400

    for each working

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -67,7 +67,12 @@ class BSTNode:
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        fn(self.value)
+        if self.right:
+            self.right.for_each(fn)
+        if self.left:
+            self.left.for_each(fn)
+
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
